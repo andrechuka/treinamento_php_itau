5e5ca4669439bc958c857b73a2e54562047fd793
andrechuka <73255824+andrechuka@users.noreply.github.com>
2020-10-29 14:35:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php $i = (9+1); ?>
 <h1>Bem vindo turma <?php echo "$i B"; ?> com professor Danilo Aparecido</h1>
 
-<h1>Primeira aula<?php echo "$i B"; ?> Rumo ao desafio</h1>
+<h1>Primeira aula de PHP da turma<?php echo "$i B"; ?>-- Rumo ao desafio</h1>
